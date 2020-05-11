36915f1927c2a0f59d7c715f384dce746a4e6669
nami2012 <namithasadeeshbabu@gmail.com>
2020-05-11 18:31:57 +0530

    add Operating System name

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "ncurses_display.h"
 #include "system.h"
-
+#include<iostream>
 int main() {
   System system;
-  NCursesDisplay::Display(system);
+  std::cout<<system.OperatingSystem();
+  //NCursesDisplay::Display(system);
 }
